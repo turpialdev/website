03fe657e4058a1a1776b48ecc8a73a1dbf58f99b
Gustavo <gsantamaria@turpialdev.com>
2018-08-18 17:34:01 -0400

    menu bar

--- assets/js/base.js
+++ assets/js/base.js
@@ -4,12 +4,14 @@ $(function() {
           $('body').addClass('onBodyColorChange');
           $('header').addClass('onBodyColorChange');
           $('footer').addClass('onBodyColorChange');
+          $('.subsection').addClass('onBodyColorChange');
           $('.nav-logo img').addClass('grayScale');
        }
        if ($(this).scrollTop() < 50) {
           $('body').removeClass('onBodyColorChange');
           $('header').removeClass('onBodyColorChange');
           $('footer').removeClass('onBodyColorChange');
+          $('.subsection').removeClass('onBodyColorChange');
           $('.nav-logo img').removeClass('grayScale');
        }
     });
