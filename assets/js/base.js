d99e96a46fa0b25081fb26a2ed12d902956f2568
Keiber Prin <kprin@turpialdev.com>
2018-08-28 15:18:14 -0400

    - Code cleaning and refactor - Opportunities page now sets the border gray without scrolling

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,18 +1,26 @@
 $(function() {
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 50) {
-          $('body').addClass('onBodyColorChange');
-          $('header').addClass('onBodyColorChange');
-          $('footer').addClass('onBodyColorChange');
-          $('.subsection').addClass('onBodyColorChange');
-          $('.nav-logo img').addClass('grayScale');
-       }
-       if ($(this).scrollTop() < 50) {
-          $('body').removeClass('onBodyColorChange');
-          $('header').removeClass('onBodyColorChange');
-          $('footer').removeClass('onBodyColorChange');
-          $('.subsection').removeClass('onBodyColorChange');
-          $('.nav-logo img').removeClass('grayScale');
-       }
-    });
+    if(window.location.href.indexOf("opportunities") > -1) {
+        $('body').addClass('onBodyColorChange');
+        $('header').addClass('onBodyColorChange');
+        $('footer').addClass('onBodyColorChange');
+        $('.subsection').addClass('onBodyColorChange');
+        $('.nav-logo img').addClass('grayScale');
+    } else {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('body').addClass('onBodyColorChange');
+                $('header').addClass('onBodyColorChange');
+                $('footer').addClass('onBodyColorChange');
+                $('.subsection').addClass('onBodyColorChange');
+                $('.nav-logo img').addClass('grayScale');
+            }
+            if ($(this).scrollTop() < 50) {
+                $('body').removeClass('onBodyColorChange');
+                $('header').removeClass('onBodyColorChange');
+                $('footer').removeClass('onBodyColorChange');
+                $('.subsection').removeClass('onBodyColorChange');
+                $('.nav-logo img').removeClass('grayScale');
+            }
+        });
+    }
  });
